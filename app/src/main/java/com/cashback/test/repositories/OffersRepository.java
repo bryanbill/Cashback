369779275424ca7db46c521c97f31e1650298645
Restyled.io <commits@restyled.io>
2022-11-23 22:17:25 +0000

    Restyled by clang-format

--- app/src/main/java/com/cashback/test/repositories/OffersRepository.java
+++ app/src/main/java/com/cashback/test/repositories/OffersRepository.java
@@ -2,101 +2,101 @@ package com.cashback.test.repositories;
 
 import android.content.Context;
 import android.os.AsyncTask;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-
 import com.cashback.test.database.DatabaseClient;
 import com.cashback.test.interfaces.IOffers;
 import com.cashback.test.models.OfferModel;
 import com.cashback.test.utils.FilterUtil;
-
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OffersRepository {
-    private static final String API = "https://cashbacktest.s3.eu-west-2.amazonaws.com/";
+  private static final String API =
+      "https://cashbacktest.s3.eu-west-2.amazonaws.com/";
 
-    private IOffers iOffers;
-    private MutableLiveData<List<OfferModel>> offersResponseLiveData;
-    private Context context;
+  private IOffers iOffers;
+  private MutableLiveData<List<OfferModel>> offersResponseLiveData;
+  private Context context;
 
-    public OffersRepository(Context ctx) {
-        offersResponseLiveData = new MutableLiveData<>();
-        this.context = ctx;
+  public OffersRepository(Context ctx) {
+    offersResponseLiveData = new MutableLiveData<>();
+    this.context = ctx;
 
-        OkHttpClient client = new OkHttpClient.Builder().build();
+    OkHttpClient client = new OkHttpClient.Builder().build();
 
-        iOffers = new retrofit2.Retrofit.Builder()
-                .baseUrl(API)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(IOffers.class);
+    iOffers = new retrofit2.Retrofit.Builder()
+                  .baseUrl(API)
+                  .client(client)
+                  .addConverterFactory(GsonConverterFactory.create())
+                  .build()
+                  .create(IOffers.class);
+  }
+
+  public void getOffers() throws ExecutionException, InterruptedException {
+    // Retrieve local data
+    class GetOffers extends AsyncTask<Void, Void, List<OfferModel>> {
+
+      @Override
+      protected List<OfferModel> doInBackground(Void... voids) {
+        List<OfferModel> offers = DatabaseClient.getInstance(context)
+                                      .getAppDatabase()
+                                      .dataBaseAction()
+                                      .getAllOffers();
+        return offers;
+      }
+
+      @Override
+      protected void onPostExecute(List<OfferModel> offers) {
+        super.onPostExecute(offers);
+        offersResponseLiveData.setValue(FilterUtil.filterByDateAsc(offers));
+      }
     }
 
-    public void getOffers() throws ExecutionException, InterruptedException {
-        //Retrieve local data
-        class GetOffers extends AsyncTask<Void, Void, List<OfferModel>> {
+    GetOffers getOffers = new GetOffers();
+    List<OfferModel> offers = getOffers.execute().get();
 
-            @Override
-            protected List<OfferModel> doInBackground(Void... voids) {
-                List<OfferModel> offers = DatabaseClient
-                        .getInstance(context)
-                        .getAppDatabase()
-                        .dataBaseAction().getAllOffers();
-                return offers;
-            }
+    if (offers == null) {
+      iOffers.getOffers().enqueue(new Callback<List<OfferModel>>() {
+        @Override
+        public void onResponse(Call<List<OfferModel>> call,
+                               retrofit2.Response<List<OfferModel>> response) {
+          if (response.body() != null) {
+            List<OfferModel> offers = response.body();
+            offersResponseLiveData.postValue(
+                FilterUtil.filterByDateAsc(offers));
 
-            @Override
-            protected void onPostExecute(List<OfferModel> offers) {
-                super.onPostExecute(offers);
-                offersResponseLiveData.setValue(FilterUtil.filterByDateAsc(offers));
+            class SaveOffers extends AsyncTask<Void, Void, Void> {
+              @Override
+              protected Void doInBackground(Void... voids) {
+                // Persist data to database
+                for (OfferModel offer : offers) {
+                  DatabaseClient.getInstance(context)
+                      .getAppDatabase()
+                      .dataBaseAction()
+                      .insertOffer(offer);
+                }
+                return null;
+              }
             }
+            SaveOffers saveOffers = new SaveOffers();
+            saveOffers.execute();
+          }
         }
 
-        GetOffers getOffers = new GetOffers();
-        List<OfferModel> offers = getOffers.execute().get();
-
-        if (offers == null){
-            iOffers.getOffers()
-                    .enqueue(new Callback<List<OfferModel>>() {
-                        @Override
-                        public void onResponse(Call<List<OfferModel>> call, retrofit2.Response<List<OfferModel>> response) {
-                            if (response.body() != null) {
-                                List<OfferModel> offers = response.body();
-                                offersResponseLiveData.postValue(FilterUtil.filterByDateAsc(offers));
-
-                                class SaveOffers extends AsyncTask<Void, Void, Void> {
-                                    @Override
-                                    protected Void doInBackground(Void... voids) {
-                                        // Persist data to database
-                                        for (OfferModel offer : offers) {
-                                            DatabaseClient.getInstance(context).getAppDatabase().dataBaseAction().insertOffer(offer);
-                                        }
-                                        return null;
-                                    }
-                                }
-                                SaveOffers saveOffers = new SaveOffers();
-                                saveOffers.execute();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<List<OfferModel>> call, Throwable t) {
-                            offersResponseLiveData.postValue(null);
-                        }
-                    });
+        @Override
+        public void onFailure(Call<List<OfferModel>> call, Throwable t) {
+          offersResponseLiveData.postValue(null);
         }
+      });
     }
+  }
 
-    public LiveData<List<OfferModel>> listOffers() {
-        return offersResponseLiveData;
-    }
-
+  public LiveData<List<OfferModel>> listOffers() {
+    return offersResponseLiveData;
+  }
 }
--- app/src/main/java/com/cashback/test/utils/FilterUtil.java
+++ app/src/main/java/com/cashback/test/utils/FilterUtil.java
@@ -2,9 +2,7 @@ package com.cashback.test.utils;
 
 import android.os.Build;
 import android.util.Log;
-
 import com.cashback.test.models.OfferModel;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -13,32 +11,35 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FilterUtil {
-    public static List<OfferModel> filterByDateAsc(List<OfferModel> offers) {
-        // Remove offers whose getOfferEndDate is after today
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            List<OfferModel> filteredOffers = offers.stream()
-                    .filter(offer -> {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                        Date today = new Date();
-                        Date offerEndDate = null;
-                        try {
-                            offerEndDate = sdf.parse(offer.getOfferEndDate());
-                        } catch (ParseException e) {
-                            e.printStackTrace();
-                        }
-                        return offerEndDate != null && offerEndDate.after(today);
-                    })
-                    .collect(Collectors.toList());
-
-            // Sort the filtered offers by promotionDetailsRanking
-            filteredOffers.sort((o1, o2) -> {
-                if (o1.getPromotionDetailsRanking() == null || o2.getPromotionDetailsRanking() == null) {
-                    return 0;
+  public static List<OfferModel> filterByDateAsc(List<OfferModel> offers) {
+    // Remove offers whose getOfferEndDate is after today
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+      List<OfferModel> filteredOffers =
+          offers.stream()
+              .filter(offer -> {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date today = new Date();
+                Date offerEndDate = null;
+                try {
+                  offerEndDate = sdf.parse(offer.getOfferEndDate());
+                } catch (ParseException e) {
+                  e.printStackTrace();
                 }
-                return o1.getPromotionDetailsRanking().compareTo(o2.getPromotionDetailsRanking());
-            });
-            return filteredOffers;
+                return offerEndDate != null && offerEndDate.after(today);
+              })
+              .collect(Collectors.toList());
+
+      // Sort the filtered offers by promotionDetailsRanking
+      filteredOffers.sort((o1, o2) -> {
+        if (o1.getPromotionDetailsRanking() == null ||
+            o2.getPromotionDetailsRanking() == null) {
+          return 0;
         }
-        return offers;
+        return o1.getPromotionDetailsRanking().compareTo(
+            o2.getPromotionDetailsRanking());
+      });
+      return filteredOffers;
     }
+    return offers;
+  }
 }
