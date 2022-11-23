7161ee821acabe8a7c847376c447d25c2ea36d70
bryanbill <emobrya@gmail.com>
2022-11-23 22:11:52 +0300

    ft:Database - abstracts room db

---
+++ app/src/main/java/com/cashback/test/database/Database.java
@@ -0,0 +1,32 @@
+package com.cashback.test.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+import com.cashback.test.models.OfferModel;
+
+
+@androidx.room.Database(entities = {OfferModel.class}, version = 1, exportSchema = false)
+public abstract class Database extends RoomDatabase {
+    private static volatile Database INSTANCE;
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
+}
